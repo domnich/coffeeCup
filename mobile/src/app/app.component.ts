f8d4841f12ebda15814571796e8c88eb97e72494
iDomnich <domnich.ivan@gmail.com>
2017-11-06 14:57:49 +0200

    commit

--- mobile/src/app/app.component.ts
+++ mobile/src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { Platform, Nav, Config  } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
@@ -10,6 +10,12 @@ import { HomePage } from '../pages/home/home';
 export class MyApp {
   rootPage:any = HomePage;
 
+  @ViewChild(Nav) nav: Nav;
+
+    pages: any[] = [
+        { title: 'Home', component: HomePage }
+    ]
+
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
@@ -18,5 +24,11 @@ export class MyApp {
       splashScreen.hide();
     });
   }
+
+    openPage(page) {
+        // Reset the content nav to have just this page
+        // we wouldn't want the back button to show in this scenario
+        this.nav.setRoot(page.component);
+    }
 }
 
