e04a9cfbffae9ccfb7155d191d7ef9e7ca4259b4
iDomnich <domnich.ivan@gmail.com>
2017-12-05 15:37:16 +0200

    jquery

--- mobile/src/components/wheel-selector/wheel-selector.ts
+++ mobile/src/components/wheel-selector/wheel-selector.ts
@@ -1,6 +1,6 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, ViewChild, ngAfterViewInit } from '@angular/core';
 import {Gesture} from 'ionic-angular/gestures/gesture';
-
+import * as $ from 'jquery';
 @Component({
   selector: 'wheel-selector',
   templateUrl: 'wheel-selector.html'
@@ -11,50 +11,73 @@ export class WheelSelectorComponent {
   @ViewChild('slideList') slideList;
   pressGesture: Gesture;
   startCounter: number;
+  names:Array<string>;
+  list: ElementRef;
   constructor() {
     console.log('Hello WheelSelectorComponent Component');
 
 
+this.names = ['Эспрессо', 'Американо', 'Латте', 'Какао', 'Чай']
+
     this.startCounter = 0;
 
-setTimeout(() => {
-    console.log(this.wheel.nativeElement, 2);
+  }
 
-    console.log(this.slideList)
+  ngAfterViewInit() {
+      this.setSliderWidth();
+  }
 
-    this.pressGesture = new Gesture(this.wheel.nativeElement);
-            this.pressGesture.listen();
+  setSliderWidth() {
+      let width = 0,
+      this.list = $(this.wheel.nativeElement).find('.common-list');
+      this.list.find('>li').each(function() {
+        width += $(this).outerWidth() + parseInt($(this).css('margin-right'));
+      });
+      width = +width.toFixed(0);
+      this.list.css({
+        width: width
+      });
 
 
-    this.pressGesture.on('pan', e => {
 
-        console.log(e)
-        if(e.direction === 2) {
-            this.startCounter--;
-        } else if(e.direction === 4) {
-            this.startCounter++;
-        }
 
-        if(e.isFinal) {
-         this.slideList.nativeElement.style.transform = 'translateX(0px)';
-         this.slideList.nativeElement.style.transition = 'transform 300ms';
-          this.startCounter = 0;
-        } else {
-            this.slideList.nativeElement.style.transform = 'translate('+ this.startCounter*6 +'px, 0)';
-            this.slideList.nativeElement.style.transition = 'transform 0ms';
-        }
+      this.activateSlider();
+  }
 
-        // if(e.isFirst) {
-        //   console.log(this.startCounter , 'FIRST')
-        // } else if(e.isFinal) {
-        //     console.log(e.center.x , 'LAST')
-        // } else {
-        //     console.log(e.center.x, '=======')
-        // }
-    })
+  activateSlider() {
 
-}, 2000);
 
+
+        this.pressGesture = new Gesture(this.wheel.nativeElement);
+                this.pressGesture.listen();
+
+
+        this.pressGesture.on('pan', e => {
+
+            if(e.direction === 2) {
+                this.startCounter--;
+            } else if(e.direction === 4) {
+                this.startCounter++;
+            }
+
+            if(e.isFinal) {
+                this.findActiveItem();
+          //   this.slideList.nativeElement.style.transform = 'translateX(0px)';
+            // this.slideList.nativeElement.style.transition = 'transform 500ms';
+          //    this.startCounter = 0;
+
+            } else {
+                this.slideList.nativeElement.style.transform = 'translate('+ this.startCounter*5 +'px, 0)';
+                this.slideList.nativeElement.style.transition = 'transform 0ms';
+            }
+        });
+
+  }
+
+  findActiveItem() {
+    this.list.find('>li').each(function() {
+      console.log($(this).position().left)
+    });
   }
 
 
