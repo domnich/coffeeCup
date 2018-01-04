0a2b856fd7117f05cea72c2c5873f56d22d42ce4
iDomnich <domnich.ivan@gmail.com>
2018-01-04 13:50:45 +0200

    Search results component implementation

--- mobile/src/components/search-results/search-results.ts
+++ mobile/src/components/search-results/search-results.ts
@@ -1,26 +1,50 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
+import { Content } from 'ionic-angular';
 import * as $ from 'jquery';
 import { DataService } from '../../providers/shared/shared.service';
 import { Cafe, FilteredCafes } from "../../models/cafe.interface";
 import {Observable} from "rxjs/Observable";
-import { Keyboard } from '@ionic-native/keyboard';
-import { Content } from 'ionic-angular';
+
 
 @Component({
   selector: 'search-results',
   templateUrl: 'search-results.html'
 })
 export class SearchResultsComponent {
-  @ViewChild('Content') content: Content;
+
   public showResultsContainer: boolean = true;
   public cafes: Cafe[] = [];
-  constructor(private element: ElementRef, private shareData: DataService, private keyboard: Keyboard) {
-    console.log(this.content, 'CONTENT')
+  @ViewChild('galleryContainer') galleryContainer: ElementRef;
+  constructor(private element: ElementRef, private shareData: DataService) {
+    console.log(this.galleryContainer, 'galleryContainer')
     this.shareData.filteredCafesSubscriber.subscribe((obj: FilteredCafes) => {
       this.showResultsContainer = !!(Object.keys(obj) && Object.keys(obj).length && (obj.data.length || obj.value.length));
       this.cafes = obj.data ? obj.data : [];
       console.log(obj.data, 'obj.data');
+      if(this.cafes.length) {
+    
+      }
+           
     });
+    console.log(this, 'thissss')
+
+
+  
+
+    
+  }
+
+  ngAfterViewInit() {
+    console.log('TADA111M');
+    this.galleryContainer.ionScroll.subscribe((event) => {
+          console.log(123);
+        $('.searchbar-input').focus();
+      })
+   
+      // this.content.ionScroll.subscribe((event) => {
+      //     console.log(123);
+      //   $('.searchbar-input').focus();
+      // })
   }
  
 }
