f684333b2bb715af79393322a4a1c0b69b9c79f4
iDomnich <domnich.ivan@gmail.com>
2018-01-04 13:02:56 +0200

    Search bar logic implementation

--- mobile/src/components/search-results/search-results.ts
+++ mobile/src/components/search-results/search-results.ts
@@ -1,21 +1,26 @@
-import { Component, ElementRef } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import * as $ from 'jquery';
 import { DataService } from '../../providers/shared/shared.service';
 import { Cafe, FilteredCafes } from "../../models/cafe.interface";
 import {Observable} from "rxjs/Observable";
+import { Keyboard } from '@ionic-native/keyboard';
+import { Content } from 'ionic-angular';
 
 @Component({
   selector: 'search-results',
   templateUrl: 'search-results.html'
 })
 export class SearchResultsComponent {
+  @ViewChild('Content') content: Content;
   public showResultsContainer: boolean = true;
   public cafes: Cafe[] = [];
-  constructor(private element: ElementRef, private shareData: DataService) {
+  constructor(private element: ElementRef, private shareData: DataService, private keyboard: Keyboard) {
+    console.log(this.content, 'CONTENT')
     this.shareData.filteredCafesSubscriber.subscribe((obj: FilteredCafes) => {
       this.showResultsContainer = !!(Object.keys(obj) && Object.keys(obj).length && (obj.data.length || obj.value.length));
       this.cafes = obj.data ? obj.data : [];
       console.log(obj.data, 'obj.data');
     });
   }
+ 
 }
