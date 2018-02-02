234bf435b6cbf0a50ef7f3870d758dd8e3f42958
iDomnich <domnich.ivan@gmail.com>
2018-02-02 11:22:39 +0200

    Facebook login

--- mobile/src/app/app.module.ts
+++ mobile/src/app/app.module.ts
@@ -4,15 +4,16 @@ import {IonicApp, IonicErrorHandler, IonicModule} from 'ionic-angular';
 import {SplashScreen} from '@ionic-native/splash-screen';
 import {StatusBar} from '@ionic-native/status-bar';
 import { CommonModule } from "@angular/common";
+import { Facebook } from '@ionic-native/facebook';
+
 import {MyApp} from './app.component';
 import {DataProvider} from '../providers/data/data';
 import {HttpModule} from "@angular/http";
 import { AppEmitterProvider } from '../providers/app-emitter/app-emitter';
 import {Keyboard} from "@ionic-native/keyboard";
-
 import {CitySelectComponent} from "../components/city-select/city-select";
-
 import { DataService } from '../providers/shared/shared.service';
+
 
 @NgModule({
     declarations: [
@@ -42,7 +43,8 @@ import { DataService } from '../providers/shared/shared.service';
         SplashScreen,
         {provide: ErrorHandler, useClass: IonicErrorHandler},
         DataProvider,
-        AppEmitterProvider
+        AppEmitterProvider,
+        Facebook
     ]
 })
 export class AppModule {
--- mobile/src/pages/login/login.ts
+++ mobile/src/pages/login/login.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
+import { Facebook, FacebookLoginResponse } from '@ionic-native/facebook';
 
 @IonicPage()
 @Component({
@@ -9,11 +9,23 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class LoginPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams,
+    private fb: Facebook
+  ) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad LoginPage');
+  }
+
+  facebookLogin() {
+    this.fb.login(['public_profile', 'user_friends', 'email'])
+    .then((res: FacebookLoginResponse) => {
+      console.log('Logged into Facebook!', res);
+    })
+    .catch(e => console.log('Error logging into Facebook', e));
   }
 
 }
