af8feedf349a40cb8bce158d1a4bd4fae71a49eb
iDomnich <domnich.ivan@gmail.com>
2018-02-04 16:17:56 +0200

    Removed unused component

--- mobile/src/pages/about/about.module.ts
+++ mobile/src/pages/about/about.module.ts
@@ -1,15 +1,13 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { AboutPage } from './about';
-import {CafeListModule} from "../../components/cafe-list/cafe-list.module";
 
 @NgModule({
   declarations: [
     AboutPage,
   ],
   imports: [
-    IonicPageModule.forChild(AboutPage),
-      CafeListModule
+    IonicPageModule.forChild(AboutPage)
   ],
 })
 export class AboutPageModule {}
